fe818bede8d011399f41a83bf4f0f4ceac9fa197
Hugo Goes <hugo.henkel@hotmail.com>
2020-08-12 12:18:38 -0300

    initial structure

---
+++ src/main/java/com/springbatchproject/springbatchusage/SpringBatchUsageApplication.java
@@ -0,0 +1,13 @@
+package com.springbatchproject.springbatchusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbatchproject/springbatchusage/SpringBatchUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatchproject.springbatchusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
